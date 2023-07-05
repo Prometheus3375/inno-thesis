814ba1e254c94a29d6ce62133f138e2fd6814c52
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2023-07-05 19:43:40 +0300

    Initial thesis commit
    Implemented sector drawing and figures.

---
+++ Python3/Thesis/functions.py
@@ -0,0 +1,42 @@
+from typing import List, Union
+from common import Real
+from point import Point
+
+
+def binomial_coefficients(degree: int) -> List[int]:
+    if degree < 0:
+        raise ValueError(f'Degree must be non-negative, got {degree}')
+    if degree == 0:
+        return [1]
+    if degree == 1:
+        return [1, 1]
+    prev = [1, 1]
+    for d in range(2, degree + 1):
+        curr = [1]
+        for i in range(1, d):
+            curr.append(prev[i - 1] + prev[i])
+        curr.append(1)
+        prev = curr
+    return prev
+
+
+def bezier(t: Real, *values: Union[Real, Point]) -> Union[Real, Point]:
+    if not (0 <= t <= 1):
+        raise ValueError(f'Parameter must be in range [0, 1], got {t}')
+
+    n = len(values) - 1
+    if n < 1:
+        raise ValueError(f'Number of control points must be greater than 1, got {n + 1}')
+
+    mt = 1 - t
+    w = binomial_coefficients(n)
+    ans = 0
+    for i, v in enumerate(values):
+        ans += v * t ** i * mt ** (n - i) * w[i]
+
+    return ans
+
+
+def qbezeir_svg_given_middle(p0: Point, p2: Point, pm: Point) -> str:
+    p1 = (pm - p0 / 4 - p2 / 4) * 2
+    return f'Q {p1.x} {p1.y} {p2.x} {p2.y}'
---
+++ Python3/Thesis/sandbox.py
@@ -0,0 +1,33 @@
+from point import Point
+from common import *
+from figures import Sector, Circle
+import plotly.graph_objects as go
+
+
+def draw_sector(sector: Sector):
+    center = sector.center
+    view_size = sector.r * 1.5
+
+    fig = go.Figure()
+    fig.add_shape(
+        **sector.circle.as_plotly_shape(),
+        line_color='Green',
+    )
+    fig.add_shape(
+        **sector.as_plotly_shape(),
+        line_color='RoyalBlue',
+    )
+    fig.update_xaxes(
+        showgrid=False,
+        zeroline=False,
+        range=[center.x - view_size, center.x + view_size]
+    )
+    fig.update_yaxes(
+        showgrid=False,
+        zeroline=False,
+        scaleanchor='x',
+        scaleratio=1,
+        range=[center.y - view_size, center.y + view_size]
+    )
+    # fig.update_layout(width=900, height=900, )
+    fig.show()
